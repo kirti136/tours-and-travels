ad1f9bf73a083b0a24d72a03c90cb6e3c70b3419
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-10-29 07:54:49 +0530

    Toggle navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './app.css'
+import { Route, Routes } from "react-router-dom"
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
-    <h2>App</h2>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/main' element={<Main />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
