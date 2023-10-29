4c1b23376e0e1c4c0eb7d8c03e88f27fc0d84d57
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-10-29 11:34:42 +0530

    in[ut box compelted

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,8 +1,78 @@
 import React from "react";
 import "./home.css";
+import video from "../../Assets/video01.mp4";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa";
+import { BsListUl } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+import { LiaTripadvisor } from "react-icons/lia";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className="home">
+      <div className="overlay"> </div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className="homeContent container">
+        {/* Text Div */}
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+
+          <h1 className="homeTitle">Search you Holiday</h1>
+        </div>
+
+        {/* Inner Card  */}
+        <div className="cardDiv grid">
+          {/* Destination Input */}
+          <div className="destinationInput">
+            <label htmlFor="date">Search you destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter name here..." />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+          {/* Date Input */}
+          <div className="dateInput">
+            <label htmlFor="city">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          {/* Price Input */}
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max={5000} min={1000} />
+            </div>
+          </div>
+          {/* Sech Bar */}
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+
+        {/* Icons */}
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon" />
+            <LiaTripadvisor className="icon" />
+            <FaInstagram className="icon" />
+          </div>
+
+          <div className="leftIcons">
+            <BsListUl className="icon" />
+            <TbApps className="icon" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
