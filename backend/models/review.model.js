c311c532157761f5ce6ac1219bc9f502cd2e11b1
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-11-01 16:28:31 +0530

    models added

---
+++ backend/models/review.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: { type: mongoose.Types.ObjectId, ref: "Tour" },
+    username: { type: String, required: true },
+    reviewText: { type: String, required: true },
+    rating: { type: Number, required: true, min: 0, max: 5, default: 0 },
+  },
+  { timestamps: true },
+  { versionKey: false }
+);
+
+const ReviewModel = mongoose.model("Review", reviewSchema);
+
+module.exports = { ReviewModel };
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,22 +2,20 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true, trim: true },
+    username: { type: String, required: true, unique: true },
     email: {
       type: String,
       required: true,
       unique: true,
-      trim: true,
-      lowercase: true,
     },
     password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    dateOfBirth: { type: String, required: true },
+    photo: { type: String },
+    role: { type: String, default: "user" },
   },
-  {
-    versionKey: false,
-  }
+  { timestamps: true },
+  { versionKey: false }
 );
 
 const UserModel = mongoose.model("User", userSchema);
